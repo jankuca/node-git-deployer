f58e5a3d6f7a99df7a254d90d9eddc38ba9d1ec0
jankuca <jan@jankuca.com>
2011-08-05 02:46:55 +0200

    use process-input lib to get the settings

--- deploy.js
+++ deploy.js
@@ -5,10 +5,12 @@ require.paths.unshift(Path.join(__dirname, 'lib'));
 var Repository = require('node-gitrepo');
 var Deployer = require('./index');
 
+var input = require('process-input');
+
 var source_dirname = process.cwd();
 var source_repo = new Repository(source_dirname, true);
 var deployer = new Deployer(source_repo);
 
 var name = Path.basename(source_dirname, '.git');
-var target_dirname = Path.join('/var/apps', name);
+var target_dirname = Path.join(input.params.to, name);
 deployer.deployTo(target_dirname);
--- post-update-hook.sh
+++ post-update-hook.sh
@@ -5,5 +5,10 @@
 # The GIT_DIR variable overrides the directory we call "git" from in the hook.
 unset GIT_DIR
 
-# Put the correct path to the deployer here:
-node ~/deployer/deploy.js
+# Put the correct path to the deployment script here.
+# Also specify the deployment target path.
+#   The repository name will be appended
+#   If a repository called /whatever/abc.git gets updated,
+#   its branches are deployed into /var/apps/abc if you keep the setting below.
+node ~/deployer/deploy.js \
+	-to /var/apps
