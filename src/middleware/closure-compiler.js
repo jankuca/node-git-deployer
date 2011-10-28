9837f1d2cb8f8e345a7cdb7141469f37b6235dcd
Jan Kuča <jan@jankuca.com>
2011-10-28 16:03:06 +0200

    closure-compiler middleware improved, decides between the raw compiler or the calcdeps.py script based on the data in .deployerinfo.json
    If there is an "input" file specified in the compilation target sheet, the calcdeps.py script is used. It then looks for dependencies in all files from directories specified in an optional "paths" array. If there is a "$GOOG" in this array, the closure/goog directory is automatically injected in the array.

--- src/middleware/closure-compiler.js
+++ src/middleware/closure-compiler.js
@@ -38,13 +38,23 @@ Compiler.prototype.loop_ = function () {
 	var dfr = new Deferred();
 	var item = this.queue_.shift();
 	if (item) {
-		var sources = item[1]['sources'];
+		var input = item[1]['input'];
+		var paths = item[1]['paths'] || [];
+		var sources = item[1]['sources'] || [];
 		var options = item[1]['options'] || {};
-		this.runCompiler_(sources, options, item[0]).then(function () {
+
+		var next = function () {
 			this.loop_().pipe(dfr);
-		}, function (err) {
-			dfr.complete('failure', err);
-		}, this);
+		};
+
+		if (input) {
+			sources.forEach(function (source) {
+				options.push([ 'js', source ]);
+			});
+			this.runCalcdeps_(input, paths, options, item[0]).then(next, dfr, this);
+		} else {
+			this.runCompiler_(sources, options, item[0]).then(next, dfr, this);
+		}
 	} else {
 		dfr.complete('success');
 	}
@@ -52,15 +62,50 @@ Compiler.prototype.loop_ = function () {
 };
 
 /**
- * Actually runs Google Closure Compiler and handles the result
+ * Runs Google Closure Compiler
  * @param {Array.<string>} sources A list of files to compile
  * @param {!Object} options Options to pass to the compiler
  * @param {string} target The path to the compilation target
  * @return {Deferred}
  */
 Compiler.prototype.runCompiler_ = function (sources, options, target) {
-	var dfr = new Deferred();
 	var command = this.buildCompilerCommand_(sources, options, target);
+	return this.exec_(command);
+};
+
+/**
+ * Runs Google Closure Calcdeps script which runs the Compiler
+ * @param {string} input The input file
+ * @param {Array.<string>} paths A list of paths in which to look for dependencies
+ * @param {!Object} options Options to pass to the script
+ * @return {Deferred}
+ */
+Compiler.prototype.runCalcdeps_ = function (input, paths, options, target) {
+	var goog_index = paths.indexOf('$GOOG');
+	if (goog_index !== -1) {
+		paths[goog_index] = Path.join(module.exports.closure_root, 'goog');
+	}
+
+	var command = this.buildCalcdepsCommand_(input, paths, options, target);
+	var dfr = this.exec_(command);
+
+	dfr.then(function () {
+		console.info('CLOSURE COMPILER: Successfully compiled to ' + target);
+	}, function (log) {
+		console.error('CLOSURE COMPILER: Failed to compile to ' + target);
+		console.log(log);
+	});
+
+	return dfr;
+};
+
+/**
+ * Runs a compilation command
+ * @param {string} command The actual command to run
+ * @return {Deferred}
+ */
+Compiler.prototype.exec_ = function (command) {
+	var dfr = new Deferred();
 	var proc = exec(command);
 	var log = [];
 	proc.stderr.on('data', function (chunk) {
@@ -68,14 +113,9 @@ Compiler.prototype.runCompiler_ = function (sources, options, target) {
 	});
 	proc.on('exit', function (code) {
 		if (code === 0) {
-			console.info('CLOSURE COMPILER: Successfully compiled to ' + target);
 			dfr.complete('success');
 		} else {
-			console.error('CLOSURE COMPILER: Failed to compile to ' + target);
-			log.forEach(function (line) {
-				console.error(line);
-			});
-			dfr.complete('failure');
+			dfr.complete('failure', log.join(''));
 		}
 	});
 	return dfr;
@@ -88,11 +128,11 @@ Compiler.prototype.runCompiler_ = function (sources, options, target) {
  * @param {string} target The path to the compilation target
  * @return {string} The command
  */
- Compiler.prototype.buildCompilerCommand_ = function (sources, options, target) {
+Compiler.prototype.buildCompilerCommand_ = function (sources, options, target) {
 	var root = this.root_;
 	var command = 'java -jar ' + Path.join(module.exports.closure_root, 'bin', 'compiler.jar');
 	sources.forEach(function (source) {
-		command += ' --js ' + Path.join(root, source);
+		command += ' --js ' + Path.resolve(root, source);
 	});
 	command += ' --js_output_file ' + Path.join(root, target);
 	options.forEach(function (option) {
@@ -101,6 +141,33 @@ Compiler.prototype.runCompiler_ = function (sources, options, target) {
 	return command;
 };
 
+/**
+ * Returns a command to run in order to run the calcdeps.py script
+ * @param {string} input The input file
+ * @param {Array.<string>} sources A list of files to compile
+ * @param {!Object} options Options to pass to the compiler
+ * @param {string} target The path to the compilation target
+ */
+Compiler.prototype.buildCalcdepsCommand_ = function (input, paths, options, target) {
+	var root = this.root_;
+
+	var command = Path.join(module.exports.closure_root, 'bin', 'calcdeps.py');
+	command += ' --output_mode compiled'
+	command += ' --compiler_jar ' + Path.join(module.exports.closure_root, 'bin', 'compiler.jar');
+
+	paths.forEach(function (path) {
+		command += ' --path ' + Path.resolve(root, path);
+	});
+	command += ' --input ' + Path.join(root, input);
+
+	options.forEach(function (option) {
+		command += ' --compiler_flags "--' + option[0] + '=' + option[1].replace('"', '\\"') + '"';
+	});
+
+	command += ' > ' + Path.join(root, target);
+
+	return command;
+};
 
 /**
  * Initiates the middleware
